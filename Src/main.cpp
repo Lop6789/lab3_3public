65d51bac6c7bbccda577bdc301df795faa5deb4f
Lop6789 <Dsshnyakin@yandex.ru>
2023-01-22 21:15:26 +0300

    All works fine

--- Src/main.cpp
+++ Src/main.cpp
@@ -8,10 +8,11 @@ int main(){
     int flag = 0;
     int res = 0;
     while (!flag){
-        cout << "\nChoose action:\n1)Histogram\n2)Test Histogram\n3)Test AVLMap\n0)Exit\n\n";
+        cout << "\nChoose action:\n1)Print Incedent Matrix\n2)Find Conneceted Components\n3)Find the shortest way by Dijkstra\n0)Exit\n\n";
         cin >> res;
         switch (res){
             case 1:{
+                
                 break;
             }
             case 2:{
