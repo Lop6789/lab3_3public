d84bff05a83ff66cd13756690a168bfe2fabfdab
Lop6789 <Dsshnyakin@yandex.ru>
2023-01-22 16:27:48 +0300

    idk

--- Src/test.cpp
+++ Src/test.cpp
@@ -4,6 +4,7 @@
 #include "../Headers/Sequence/LinkedListSeq.hpp"
 
 using namespace std;
+
 void print_int_graph(const Graph<int, int>& g);
 void print_list(const LinkedListSequence<Edge<int>>& lseq);
 
@@ -14,19 +15,6 @@ int cmp (const Pair<Vertex<int>, LinkedListSequence<Edge<int>>>& p1, const Pair<
 int main(){
     int a[] ={1,2,3,4,5,6,7,8,9};
     LinkedListSequence list(a, 9);
-    // }
-    // list.Delete(8);
-    // list.Append(100);
-    // list.print();
-    // for (int i = 0; i < list.GetLength(); i++){
-    //     cout << list.Get(i) << endl;
-    // }
-
-
-    //  IMap<int, LinkedList<int>>* imap = new AVLMap<int, int>(&cmp1);
-    //  imap->Add(1,3);
-    //  imap->Add(13,31);
-    //  imap->Add(100,300);
 
     Graph <int, int> g (cmp);
 
@@ -35,10 +23,10 @@ int main(){
     Vertex<int> v3(3);
     Vertex<int> v4(4);
 
-    Edge<int> e1(1, 1, 2);
-    Edge<int> e2(2, 2, 3);
-    Edge<int> e3(3, 3, 4);
-    Edge<int> e4(4, 4, 1);
+    Edge<int> e1(1, 1, 2, 0);
+    Edge<int> e2(2, 2, 3, 0);
+    Edge<int> e3(3, 3, 4, 0);
+    Edge<int> e4(4, 4, 1, 0);
 
     g.AddSingleVertex(v1);
     g.AddSingleVertex(v2);
@@ -52,19 +40,11 @@ int main(){
     print_int_graph(g);
 
 
-    // g.AddSingleVertex(v1);
-
     Sequence<int>* seq = g.GetVertexesIds();
 
-    for (int i = 0; i < seq->GetLength(); i++){
-        cout << seq->Get(i) << endl;
-    }
-
-
-
-
-    // IncList<int, int> graph(imap);
-    
+    // for (int i = 0; i < seq->GetLength(); i++){
+    //     cout << seq->Get(i) << endl;
+    // }
 
     delete seq;
     return 0;
@@ -87,8 +67,80 @@ void print_list(const LinkedListSequence<Edge<int>>& lseq){
     else {
         cout << "[ ";
             for (int i = 0; i < lseq.GetLength(); i++) {
-                cout << lseq.Get(i).GetStartId() << ","<< lseq.Get(i).GetEndId() << " ";
+                cout << "(" << lseq.Get(i).GetStartId() << ","<< lseq.Get(i).GetEndId() << "," << lseq.Get(i).GetData() << ") " ;
             }
         cout << "]" << endl;
     }
 }
+
+// void init (int Vert){
+    // for 
+// }
+// Sequence<int>* s;
+// Sequence<int>* p;
+// Sequence<bool>* used;
+// // int s[] = {}
+// // int d[] = {}; // расстояния 
+// const int INF = 1000000;
+
+// void Dijkstra(int vertexesQty, int vertexStartId){
+
+//     s = new ArraySequence<int>();
+//     p = new ArraySequence<int>();
+//     used = new ArraySequence<bool>();
+//     //INIT:
+//     for (int i = 0; i < vertexesQty; i++){
+//         s->Append(INF);
+//         p->Append(-1);
+//         used->Append(false);
+//     }
+//     s->Set(vertexStartId, 0);
+//     p->Set(vertexStartId, vertexStartId);
+//     //ALGO:
+
+//     for (int i = 0; i < vertexesQty; i++){
+//         int v = -1;
+//         for (int j = 0; j < vertexesQty; j++){
+//            if (!(used->Get(j)) && (v == -1 || (s->Get(j) < s->Get(v)))){
+//                 v = j;
+//            } 
+
+//            if (s->Get(v) == INF) break;
+//            used->Set(v, true);
+//            //смежные вершины
+//            //
+
+//         }
+//     }
+
+
+// Sequence<int>* dist;
+// Sequence<int>* p;
+// Sequence<bool>* use;
+
+// void  Dekstra(int vertexesQty, int vertexStartId, const Graph<int,int>& graph) {
+//     dist = new ArraySequence<int>();
+//     use = new ArraySequence<bool>();
+
+//     for (int i = 0; i < vertexesQty; i++){
+//         dist->Append(INF);
+//         use->Append(false);
+//     }
+
+//     dist->Set(vertexStartId, 0);
+
+//     for (int i = 0; i < vertexesQty; i++){
+//         v = -1;
+//         for (int j = 0; j < vertexesQty; j++){
+//             if (!(use->Get(j)) && v==-1 || (dist->Get(j) < dist->Get(v)));
+//                 v = j;
+//         }
+//         if (dist->Get(v) == INF) break;
+//         use->Set(v, true);
+        
+        
+//         Sequence<int>* adjVertexes = graph.GetAdjVert
+//     }
+
+
+// }
